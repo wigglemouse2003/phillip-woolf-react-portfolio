d95fc985efb1049f0b976ab65fe5f7f977efa2bb
Phillip <wigglemice@gmail.com>
2019-10-12 17:46:36 -0600

    fixing typeError error

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -3,9 +3,6 @@ import axios from "axios";
 import DropzoneComponent from "react-dropzone-component";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
-import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
-
 import RichTextEditor from "../forms/rich-text-editor";
 
 export default class BlogForm extends Component {
