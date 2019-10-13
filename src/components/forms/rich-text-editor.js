06fa5aa5d28e82a9b618928c20112602ea1893e5
Phillip <wigglemice@gmail.com>
2019-10-12 19:14:43 -0600

    fixing typeError error

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { EditorState, convertToRaw, ContentState } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
+import Editor from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
 
@@ -55,7 +55,25 @@ export default class RichTextEditor extends Component {
   render() {
     return (
       <div>
-        <Editor />
+        <Editor
+          editorState={this.state.editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassname="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: this.uploadFile,
+              alt: { present: true, mandatory: false },
+              previewImage: true,
+              inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
+            }
+          }}
+        />
       </div>
     );
   }
