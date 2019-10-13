2fe409d3a46ef8b3426f9e43ef2fca26f4d92a36
Phillip <wigglemice@gmail.com>
2019-10-12 18:48:25 -0600

    fixing typeError error

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -60,19 +60,6 @@ export default class RichTextEditor extends Component {
           wrapperClassName="demo-wrapper"
           editorClassname="demo-editor"
           onEditorStateChange={this.onEditorStateChange}
-          toolbar={{
-            inline: { inDropdown: true },
-            list: { inDropdown: true },
-            textAlign: { inDropdown: true },
-            link: { inDropdown: true },
-            history: { inDropdown: true },
-            image: {
-              uploadCallback: this.uploadFile,
-              alt: { present: true, mandatory: false },
-              previewImage: true,
-              inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
-            }
-          }}
         />
       </div>
     );
